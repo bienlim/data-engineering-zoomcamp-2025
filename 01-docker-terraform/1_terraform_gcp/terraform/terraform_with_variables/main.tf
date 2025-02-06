9e1e880fb844c0b0be60045de4428d55177f3a37
bien lim <bienlim.dev@gmail.com>
2025-02-06 13:27:13 +0000

    refactor: terraform variables

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
@@ -13,26 +13,31 @@ provider "google" {
   region      = var.region
 }
 
+resource "google_storage_bucket" "data-lake-bucket" {
+  name     = var.gcs_bucket_name
+  location = var.location
 
-resource "google_storage_bucket" "demo-bucket" {
-  name          = var.gcs_bucket_name
-  location      = var.location
-  force_destroy = true
+  # Optional, but recommended settings:
+  storage_class               = var.gcs_storage_class
+  uniform_bucket_level_access = true
 
+  versioning {
+    enabled = true
+  }
 
   lifecycle_rule {
-    condition {
-      age = 1
-    }
     action {
-      type = "AbortIncompleteMultipartUpload"
+      type = "Delete"
+    }
+    condition {
+      age = 30 // days
     }
   }
+
+  force_destroy = true
 }
 
-
-
-resource "google_bigquery_dataset" "demo_dataset" {
+resource "google_bigquery_dataset" "dataset" {
   dataset_id = var.bq_dataset_name
-  location   = var.location
+  project    = var.project
 }
